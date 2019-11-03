f7de614c153a839aef45b89bf02642db7e1232a7
saebyulpark <43517044+saebyulpark@users.noreply.github.com>
2019-11-03 21:56:43 +0900

    Update get_similarity_score.m

--- get_similarity_score.m
+++ get_similarity_score.m
@@ -1,16 +1,12 @@
-function [Sim]=get_similarity_score(result_matrix)
-      
+function similarity_score = get_similarity_score(result_matrix)
 
-%%
-weight_begin_num=0.5;
-weight=linspace(weight_begin_num,1,size(result_matrix,1))';
-weighted_matrix=result_matrix.*weight;
-weighted_matrix=weighted_matrix(3:end,:);
-D=mean(mean(weighted_matrix(weighted_matrix<1.1)));
-
-Sim=1-D;
-display(sprintf('Similarity score is %.4f',Sim));
-
+% Calculate the similarity score of two songs based on distance matrix.
+  weight_begin_num = 0.5;
+  weight = linspace(weight_begin_num,1,size(result_matrix,1))';
+  weighted_matrix = result_matrix.*weight;
+  weighted_matrix = weighted_matrix(3:end,:);
+  D = mean(mean(weighted_matrix(weighted_matrix<1.1)));
+  similarity_score = 1-D;
+  display(sprintf('Similarity score is %.4f',similarity_score));
+  
 end
-
-
