f1778542df8330d0f96afc39a420638245be6fc3
saebyulpark <43517044+saebyulpark@users.noreply.github.com>
2019-11-03 21:52:04 +0900

    Update main.m

--- main.m
+++ main.m
@@ -1,11 +1,13 @@
-
+% Activate path
 addpath(genpath(pwd))
- 
-song_a=readmidi('./samples/case1_song_a.mid');
-song_b=readmidi('./samples/case1_song_b.mid');
 
+% Load midi files
+song_a = readmidi('./samples/case1_song_a.mid');
+song_b = readmidi('./samples/case1_song_b.mid');
+
+% Select pitch/rhythm mode
 pitch_or_rhythm = 1; % pitch = 1, rhythm = 2
 
-D= get_distance_matrix (song_a, song_b, pitch_or_rhythm); 
-Sim=get_similarity_score(D); 
-
+% Caculate distance matrix and similarity score
+dist_mat = get_distance_matrix (song_a, song_b, pitch_or_rhythm); 
+similarity_score = get_similarity_score(dist_mat); 
